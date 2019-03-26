a3e509f9e1dd40ddeb2c77724eccf62707f8253f
M. Ali <mohammed_ali.aziz_assi@ldc.lu.se>
2019-03-26 11:30:44 +0100

    fixed untracked files

---
+++ source/var/bundles/LucatBundle/lucat_config.php
@@ -0,0 +1,82 @@
+<?php
+return $lucat = [
+    'hostname' => 'lucatopen.lu.se',
+    'port' => '1433',
+    'dbname' => 'lucatopen',
+    'username' => 'uw\\sa_lusemweb',
+    'password' => '!!a82nce0p8a!!',
+    'organisations' => [
+        [
+            'guid' => '79a58adf-a001-419b-a18c-cb8f2b7b8b87',
+            'departmentNumber' => 'v1000017',
+            'name_sv' => 'Ekonomihögskolan',
+            'name_en' => 'Lund University School of Economics and Management, LUSEM'
+        ],
+        [
+            'guid' => '2af06552-1858-42eb-9def-115a5aa31c30',
+            'departmentNumber' => 'v1000018',
+            'name_sv' => 'Centrum för ekonomisk demografi',
+            'name_en' => 'Centre for Economic Demography'
+        ],
+        [
+            'guid' => '2eac4ee0-25bd-48b0-be1d-806fe44c406d',
+            'departmentNumber' => 'v1000020',
+            'name_sv' => 'Ekonomihögskolans kansli',
+            'name_en' => "Dean's Office, LUSEM"
+        ],
+        [
+            'guid' => '69cd984d-3520-47b6-8969-36fbc4444cf3',
+            'departmentNumber' => 'v1000021',
+            'name_sv' => 'Ekonomihögskolans bibliotek',
+            'name_en' => 'School of Economics and Management Library'
+        ],
+        [
+            'guid' => '1874e9f1-4902-4134-8a32-67deb5820077',
+            'departmentNumber' => 'v1000022',
+            'name_sv' => 'Ekonomisk-historiska institutionen',
+            'name_en' => 'Department of Economic History'
+        ],
+        [
+            'guid' => 'c47f8c59-8f57-47ba-bbd5-83e9ee5858b1',
+            'departmentNumber' => 'v1000023',
+            'name_sv' => 'Företagsekonomiska institutionen',
+            'name_en' => 'Department of Business Administration'
+        ],
+        [
+            'guid' => 'a758e28c-ac3e-4411-bd0c-3bcb5b0ef0db',
+            'departmentNumber' => 'v1000025',
+            'name_sv' => 'Institutet för livsmedelsekonomisk analys, AgriFood',
+            'name_en' => 'Swedish Institute for Food and Agricultural Economics'
+        ],
+        [
+            'guid' => 'd756a6c3-c5fa-4e61-ad3f-7ec720f8aa81',
+            'departmentNumber' => 'v1000027',
+            'name_sv' => 'Institutionen för handelsrätt',
+            'name_en' => 'Department of Economics'
+        ],
+        [
+            'guid' => '3336da1e-1dd9-4f70-8869-3307a473d7b4',
+            'departmentNumber' => 'v1000028',
+            'name_sv' => 'Institutionen för informatik',
+            'name_en' => 'Department of Informatics'
+        ],
+        [
+            'guid' => '382ae8b2-018d-4c0b-9d67-5d4a6b107e05',
+            'departmentNumber' => 'v1000029',
+            'name_sv' => 'Nationalekonomiska institutionen',
+            'name_en' => 'Department of Economics'
+        ],
+        [
+            'guid' => 'ec99cf0b-cbf8-4f2c-8d3b-929a934b4c4e',
+            'departmentNumber' => 'v1000030',
+            'name_sv' => 'Statistiska institutionen',
+            'name_en' => 'Department of Statistics'
+        ],
+        [
+            'guid' => 'f1121131-a6bf-4dd0-9097-5b0aaba54667',
+            'departmentNumber' => 'v1000031',
+            'name_sv' => 'Stiftelsen Partnerskap',
+            'name_en' => 'Corporate Partnership Foundation'
+        ]
+    ]
+];
---
+++ source/var/bundles/LucrisBundle/lucris_config.php
@@ -0,0 +1,76 @@
+<?php
+return $lucris = [
+    "base_url" => "https://lucris.lub.lu.se/ws/rest",
+    "organisations" => [
+        0 => [
+            "id" => "14caa4c9-5386-4586-a411-460cd12dd8d8",
+            "sourceId" => "v1000017",
+            "name_sv" => "Ekonomihögskolan",
+            "name_en" => "Lund University School of Economics and Management, LUSEM",
+            "portalUrl" => "http://portal.research.lu.se/portal/en/organisations-units/lund-university-school-of-economics-and-management-lusem(14caa4c9-5386-4586-a411-460cd12dd8d8).html"
+        ],
+        1 => [
+            "id" => "cdc69718-dc57-49fd-b195-d1ea174f6320",
+            "sourceId" => "v1000018",
+            "name_sv" => "Centrum för ekonomisk demografi",
+            "name_en" => "Centre for Economic Demography",
+            "portalUrl" => "http://portal.research.lu.se/portal/en/organisations-units/centre-for-economic-demography(cdc69718-dc57-49fd-b195-d1ea174f6320).html"
+        ],
+        2 => [
+            "id" => "fe9411f6-3aa9-45ba-9e8c-8efddb63d7a9",
+            "sourceId" => "v1000022",
+            "name_sv" => "Ekonomisk-historiska institutionen",
+            "name_en" => "Department of Economic History",
+            "portalUrl" => "http://portal.research.lu.se/portal/en/organisations-units/department-of-economic-history(fe9411f6-3aa9-45ba-9e8c-8efddb63d7a9).html"
+        ],
+        3 => [
+            "id" => "e1623d74-136d-4437-a1ca-4172d4c7261a",
+            "sourceId" => "v1000023",
+            "name_sv" => "Företagsekonomiska institutionen",
+            "name_en" => "Department of Business Administration",
+            "portalUrl" => "http://portal.research.lu.se/portal/en/organisations-units/department-of-business-administration(e1623d74-136d-4437-a1ca-4172d4c7261a).html"
+        ],
+        4 => [
+            "id" => "d31ce7ce-07bb-43a5-bd27-ff338ec98824",
+            "sourceId" => "v1000025",
+            "name_sv" => "Institutet för livsmedelsekonomisk analys, AgriFood",
+            "name_en" => "Swedish Institute for Food and Agricultural Economics",
+            "portalUrl" => "http://portal.research.lu.se/portal/en/organisations-units/swedish-institute-for-food-and-agricultural-economics(d31ce7ce-07bb-43a5-bd27-ff338ec98824).html"
+        ],
+        5 => [
+            "id" => "fa134bc3-1b00-4580-b23d-2adcd3d725ac",
+            "sourceId" => "v1000026",
+            "name_sv" => "AgriFood Economics Centre, SLU",
+            "name_en" => "AgriFood Economics Centre, SLU",
+            "portalUrl" => "http://portal.research.lu.se/portal/en/organisations-units/agrifood-economics-centre-slu(fa134bc3-1b00-4580-b23d-2adcd3d725ac).html"
+        ],
+        6 => [
+            "id" => "7df8bc2f-f3b7-4529-a102-f3a7f53c10d7",
+            "sourceId" => "v1000027",
+            "name_sv" => "Institutionen för handelsrätt",
+            "name_en" => "Department of Business Law",
+            "portalUrl" => "http://portal.research.lu.se/portal/en/organisations-units/department-of-business-law(7df8bc2f-f3b7-4529-a102-f3a7f53c10d7).html"
+        ],
+        7 => [
+            "id" => "c37defaa-d29b-490e-bae1-d47fb8a12023",
+            "sourceId" => "v1000028",
+            "name_sv" => "Institutionen för informatik",
+            "name_en" => "Department of Informatics",
+            "portalUrl" => "http://portal.research.lu.se/portal/en/organisations-units/department-of-informatics(c37defaa-d29b-490e-bae1-d47fb8a12023).html"
+        ],
+        8 => [
+            "id" => "a91e8aa5-6143-488a-81d9-3169d2e2fad1",
+            "sourceId" => "v1000029",
+            "name_sv" => "Nationalekonomiska institutionen",
+            "name_en" => "Department of Economics",
+            "portalUrl" => "http://portal.research.lu.se/portal/en/organisations-units/department-of-economics(a91e8aa5-6143-488a-81d9-3169d2e2fad1).html"
+        ],
+        9 => [
+            "id" => "dec18c59-2f77-4982-b30a-684bcfbc5632",
+            "sourceId" => "v1000030",
+            "name_sv" => "Statistiska institutionen",
+            "name_en" => "Department of Statistics",
+            "portalUrl" => "http://portal.research.lu.se/portal/en/organisations-units/department-of-statistics(dec18c59-2f77-4982-b30a-684bcfbc5632).html"
+        ]
+    ]    
+];
