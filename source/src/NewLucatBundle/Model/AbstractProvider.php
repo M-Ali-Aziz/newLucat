9d722f2c1f7a7d4a25e5b30d24a162733c684794
M. Ali <mohammed_ali.aziz_assi@ldc.lu.se>
2019-03-27 10:30:33 +0100

    Added Lucat sync/migration

---
+++ source/src/NewLucatBundle/Model/AbstractProvider.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace NewLucatBundle\Model;
+
+use \PDO;
+use \PDOException;
+use \Exception;
+use \Pimcore\Tool\Frontend;
+
+class AbstractProvider
+{
+    protected $lucatConfigPath = PIMCORE_PRIVATE_VAR . '/bundles/NewLucatBundle/lucat_config.php';
+
+    /**
+     * Get lucat config file
+     *
+     * @return array
+     */
+    protected function getLucatConfigFile()
+    {
+        $lucatConfigPath = $this->lucatConfigPath;
+        if(file_exists($lucatConfigPath)) {
+            $lucatConfigFile = require $lucatConfigPath;
+
+            return $lucatConfigFile;
+        }
+        else {
+            // Add log to var/log/lucat.log
+            \Pimcore\Log\Simple::log("lucat", "LUCAT ERROR: Failed to get lucat config file " . __FILE__ . " Line: " . __LINE__);
+            // Exception
+            throw new Exception("LUCAT ERROR: Failed to get lucat config file" . "\n");
+        }
+    }
+
+    /**
+     * Connect to LucatOpen
+     *
+     * @return PDO object
+    */
+    protected function getLucatOpen()
+    {
+        // Get lucat config file
+        $lucatConfigFile = $this->getLucatConfigFile();
+
+        try {
+            $hostname = $lucatConfigFile['hostname'];
+            $port = $lucatConfigFile['port'];
+            $dbname = $lucatConfigFile['dbname'];
+            $username = $lucatConfigFile['username'];
+            $password = $lucatConfigFile['password'];
+
+            $lucatopen = new PDO ("dblib:host=$hostname:$port;dbname=$dbname","$username","$password");
+            
+            return $lucatopen;
+
+        } catch (PDOException $e) {
+            // Add log to var/log/lucat.log
+            \Pimcore\Log\Simple::log("lucat", "LUCAT ERROR: Failed to connect to LucatOpen: " . $e->getMessage() . " " . __FILE__ . " Line: " . __LINE__);
+            echo "Failed to connect to LucatOpen: " . $e->getMessage() . "\n";
+        }
+    }
+
+    /**
+     * Get related ParentId from Pimcore Website Settings
+     *
+     * @return string
+    */
+    protected function getParentId($configName)
+    {
+        $parentId = Frontend::getWebsiteConfig()->$configName;
+
+        if ($parentId) {
+            // Set ParentId
+            return $parentId;
+        }
+        else {
+            // Add log to var/log/lucat.log
+            \Pimcore\Log\Simple::log("lucat", "LUCAT ERROR: Failed to get ParentId for: $configName, from Website Settings! " . __FILE__ . " Line: " . __LINE__);
+
+            // Exception
+            throw new Exception("Failed to get ParentId for: $configName, from Website Settings!" . "\n");
+        }
+    }
+
+    /**
+     * Get all EHL person-uides from LucatOpen
+     *
+     * @return array
+    */
+    protected function getUids()
+    {
+        // Get departmentNumbers
+        $organisations = $this->getOrganisations();
+        $departmentNumbers = array_map(function($org) { return("'" . $org . "'"); }, $organisations);
+        $departmentNumbers = implode(',', $departmentNumbers);
+
+        // Get lucatOpen
+        $lucatopen = $this->getLucatOpen();
+
+        $sql = "SELECT "
+            . "pa.uid "
+            . "FROM luEduOrgUnit AS ou "
+            . "INNER JOIN luEduOrgRole AS ro "
+            . "ON ou.guid = ro.luEduOrgUnitGUID "
+            . "INNER JOIN luEduPersonOrgRole AS por "
+            . "ON ro.guid = por.luEduOrgRoleGUID "
+            . "INNER JOIN luEduPersonAccount AS pa "
+            . "ON por.luEduPersonGUID = pa.luEduPersonGUID "
+            . "WHERE pa.employeeType NOT LIKE ('%Student%') "
+            . "AND ou.departmentNumber IN ($departmentNumbers)";
+
+        $statement = $lucatopen->prepare($sql);
+        $statement->execute();
+        $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    /**
+     * Get all EHL departmentNumber from lucatConfigFile
+     *
+     * @return array
+    */
+    protected function getOrganisations()
+    {
+        // Get lucat config file
+        $lucatConfigFile = $this->getLucatConfigFile();
+
+        // Prepare departmentNumbers
+        $organisations = $lucatConfigFile['organisations'];
+
+        return $organisations;
+    }
+}
---
+++ source/src/NewLucatBundle/Model/OrganisationProvider.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace NewLucatBundle\Model;
+
+use NewLucatBundle\Model\AbstractProvider;
+use \Pimcore\Model\DataObject;
+use \Pimcore\Config;
+use \Pimcore\Db;
+
+/**
+ * Synchronize LucatOrganisations with LucatOpen
+ */
+class OrganisationProvider extends AbstractProvider
+{
+    public function SynchronizeLucatOrganisation()
+    {
+        // Get lucat config file
+        $lucatConfigFile = $this->getLucatConfigFile();
+
+        // Get lucatOpen
+        $lucatopen = $this->getLucatOpen();
+
+        // Prepare departmentNumbers
+        $organisations = $this->getOrganisations();
+        $departmentNumbers = array_map(function($org) { return("'" . $org . "'"); }, $organisations);
+        $departmentNumbers = implode(',', $departmentNumbers);
+
+        // Get lucatOrgParentId
+        $parentId = $this->getParentId('newLucatOrgParentId');
+
+        // Set published
+        $published = 1;
+
+        // Get the valid languages
+        $config = Config::getSystemConfig();
+        $l = explode(',', $config->general->validLanguages);
+
+        // Connenct to Pimcore DB
+        $db = Db::get();
+
+        // Fetch all organisations
+        $sql = "SELECT * "
+            . "FROM luEduOrgUnit "
+            . "WHERE departmentNumber IN ($departmentNumbers)";
+            
+        $statement = $lucatopen->prepare($sql);
+        $statement->execute();
+        $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+        // Save/Update Lucat Organisations
+        foreach ($result as $lucatOpenOrg) {
+
+            // Check if organisation allredy exists 
+            $query = sprintf('SELECT o_id FROM objects WHERE o_parentId=%s AND o_key="%s" LIMIT 0,1', $parentId, $lucatOpenOrg['departmentNumber']);
+            $oIdExisting = $db->fetchAll($query);
+            $o_id = (int) $oIdExisting[0]['o_id'];
+
+            if ($o_id) {
+                // Update the Object
+                $lucatOrgObj = DataObject\NewLucatOrganisation::getById($o_id);
+
+                $values = [
+                    'guid' => $lucatOpenOrg['guid'],
+                    'departmentNumber' => $lucatOpenOrg['departmentNumber'],
+                    'ParentDepartmentNumber' => $lucatOpenOrg['ParentDepartmentNumber'],
+                    'orgType' => $lucatOpenOrg['luEduOrgType'],
+                    'postalAdress' => $lucatOpenOrg['postalAddress'],
+                    'street' => $lucatOpenOrg['street'],
+                    'postOfficeBox' => $lucatOpenOrg['postOfficeBox'],
+                    'location' => $lucatOpenOrg['l'],
+                    'telephoneNumber' => $lucatOpenOrg['telephoneNumber'],
+                    'vxNumber' => $lucatOpenOrg['luEduOrgUnitVxNumber'],
+                    'gpsC' => $lucatOpenOrg['luEduOrgUnitGpsC'],
+                    'domain' => $lucatOpenOrg['luEduOrgUnitDomain'],
+                    'internalAdress' => $lucatOpenOrg['street'],
+                ];
+
+                $lucatOrgObj->setValues($values);
+                $lucatOrgObj->setName($lucatOpenOrg['displayName'], $l[0]);
+                $lucatOrgObj->setName($lucatOpenOrg['luEduOrgOuEN'], $l[1]);
+                $lucatOrgObj->setEducationDescription($lucatOpenOrg['luEduOrgUnitEducationDescription'], $l[0]);
+                $lucatOrgObj->setEducationDescription($lucatOpenOrg['luEduOrgUnitEducationDescriptionEN'], $l[1]);
+                $lucatOrgObj->setEducationUri($lucatOpenOrg['luEduOrgUnitEducationDescriptionURI'], $l[0]);
+                $lucatOrgObj->setEducationUri($lucatOpenOrg['luEduOrgUnitEducationDescriptionURIEN'], $l[1]);
+                $lucatOrgObj->setResearchDescription($lucatOpenOrg['luEduOrgUnitResearchDescription'], $l[0]);
+                $lucatOrgObj->setResearchDescription($lucatOpenOrg['luEduOrgUnitResearchDescriptionEN'], $l[1]);
+                $lucatOrgObj->setUrl($lucatOpenOrg['luEduOrgUnitHomePageURI'], $l[0]);
+                $lucatOrgObj->setUrl($lucatOpenOrg['luEduOrgUnitHomePageURIEN'], $l[1]);
+                $lucatOrgObj->setResearchUri($lucatOpenOrg['luEduOrgUnitResearchDescriptionURI'], $l[0]);
+                $lucatOrgObj->setResearchUri($lucatOpenOrg['luEduOrgUnitResearchDescriptionURIEN'], $l[1]);
+
+                $lucatOrgObj->save(); 
+            }
+            else {
+                // Save new Object
+                $lucatOrgObj = new DataObject\NewLucatOrganisation();
+
+                $values = [
+                    'o_parentId' => $parentId,
+                    'o_key' => $lucatOpenOrg['departmentNumber'],
+                    'o_published' => $published,
+                    'guid' => $lucatOpenOrg['guid'],
+                    'departmentNumber' => $lucatOpenOrg['departmentNumber'],
+                    'ParentDepartmentNumber' => $lucatOpenOrg['ParentDepartmentNumber'],
+                    'orgType' => $lucatOpenOrg['luEduOrgType'],
+                    'postalAdress' => $lucatOpenOrg['postalAddress'],
+                    'street' => $lucatOpenOrg['street'],
+                    'postOfficeBox' => $lucatOpenOrg['postOfficeBox'],
+                    'location' => $lucatOpenOrg['l'],
+                    'telephoneNumber' => $lucatOpenOrg['telephoneNumber'],
+                    'vxNumber' => $lucatOpenOrg['luEduOrgUnitVxNumber'],
+                    'gpsC' => $lucatOpenOrg['luEduOrgUnitGpsC'],
+                    'domain' => $lucatOpenOrg['luEduOrgUnitDomain'],
+                    'internalAdress' => $lucatOpenOrg['street'],
+                ];
+
+                $lucatOrgObj->setValues($values);
+                $lucatOrgObj->setName($lucatOpenOrg['displayName'], $l[0]);
+                $lucatOrgObj->setName($lucatOpenOrg['luEduOrgOuEN'], $l[1]);
+                $lucatOrgObj->setEducationDescription($lucatOpenOrg['luEduOrgUnitEducationDescription'], $l[0]);
+                $lucatOrgObj->setEducationDescription($lucatOpenOrg['luEduOrgUnitEducationDescriptionEN'], $l[1]);
+                $lucatOrgObj->setEducationUri($lucatOpenOrg['luEduOrgUnitEducationDescriptionURI'], $l[0]);
+                $lucatOrgObj->setEducationUri($lucatOpenOrg['luEduOrgUnitEducationDescriptionURIEN'], $l[1]);
+                $lucatOrgObj->setResearchDescription($lucatOpenOrg['luEduOrgUnitResearchDescription'], $l[0]);
+                $lucatOrgObj->setResearchDescription($lucatOpenOrg['luEduOrgUnitResearchDescriptionEN'], $l[1]);
+                $lucatOrgObj->setUrl($lucatOpenOrg['luEduOrgUnitHomePageURI'], $l[0]);
+                $lucatOrgObj->setUrl($lucatOpenOrg['luEduOrgUnitHomePageURIEN'], $l[1]);
+                $lucatOrgObj->setResearchUri($lucatOpenOrg['luEduOrgUnitResearchDescriptionURI'], $l[0]);
+                $lucatOrgObj->setResearchUri($lucatOpenOrg['luEduOrgUnitResearchDescriptionURIEN'], $l[1]);
+
+                $lucatOrgObj->save();
+            }
+        }
+    } 
+}
